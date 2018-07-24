3edcaef917519b94dcaa0fe84f67e0708beeefc3
Patrick Cemper <40242812+patrickcemper@users.noreply.github.com>
2018-07-24 19:17:18 +0200

    added primitive unit tests.

---
+++ src/test/java/patrickcemper/cloudstarx/ApplicationTest.java
@@ -0,0 +1,39 @@
+package patrickcemper.cloudstarx;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = DefaultController.class)
+public class ApplicationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testIndexPage() throws Exception {
+        mockMvc.perform(get("/index.html"))
+                .andExpect(content().string(containsString("List of services")));
+    }
+
+    @Test
+    public void testTestservice() throws Exception {
+        mockMvc.perform(get("/testservice"))
+                .andExpect(content().string(containsString("the most primitive MVC test service")));
+    }
+
+    @Test
+    public void testTestserviceWithParam() throws Exception {
+        mockMvc.perform(get("/testservice").param("someparam", "ABCDE"))
+                .andExpect(content().string(containsString("optional parameter &lt;someparam&gt;: ABCDE.")));
+    }
+
+}
