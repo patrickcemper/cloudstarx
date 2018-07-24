8b9b1532e877514be4633f11caeb28fc57a41894
Patrick Cemper <40242812+patrickcemper@users.noreply.github.com>
2018-07-24 18:35:14 +0200

    some minor refactoring of the most basic project structures.

--- src/main/java/patrickcemper/cloudstarx/Application.java
+++ src/main/java/patrickcemper/cloudstarx/Application.java
@@ -1,4 +1,4 @@
-package engineering.patrickcemper.cloudstarx;
+package patrickcemper.cloudstarx;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/patrickcemper/cloudstarx/DefaultController.java
+++ src/main/java/patrickcemper/cloudstarx/DefaultController.java
@@ -1,4 +1,4 @@
-package engineering.patrickcemper.cloudstarx;
+package patrickcemper.cloudstarx;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
