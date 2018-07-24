85000c65c8a4108cbab5861b1483956dc35a06a0
Patrick Cemper <40242812+patrickcemper@users.noreply.github.com>
2018-07-24 19:02:09 +0200

    added minimal command line runner.

--- src/main/java/patrickcemper/cloudstarx/Application.java
+++ src/main/java/patrickcemper/cloudstarx/Application.java
@@ -1,7 +1,13 @@
 package patrickcemper.cloudstarx;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
 
 /**
  * Cloud Star X
@@ -16,6 +22,22 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+            System.out.println("Command line runner started.");
+            System.out.printf("Here are some application context infos:%n" +
+                    "- application name: %s%n" +
+                    "- display name: %s%n" +
+                    "- id: %s%n" +
+                    "- startup date: %s%n",
+                    ctx.getApplicationName(),
+                    ctx.getDisplayName(),
+                    ctx.getId(),
+                    new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(ctx.getStartupDate()));
+        };
     }
 
 }
