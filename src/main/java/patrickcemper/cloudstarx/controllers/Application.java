cffef37a356360f8f1752c12ac0542decf1ff7a3
Patrick Cemper <40242812+patrickcemper@users.noreply.github.com>
2018-07-31 21:59:44 +0200

    added CORS (cross origin resource sharing) for frontend access.

--- src/main/java/patrickcemper/cloudstarx/controllers/Application.java
+++ src/main/java/patrickcemper/cloudstarx/controllers/Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import java.text.SimpleDateFormat;
@@ -19,6 +20,7 @@ import java.text.SimpleDateFormat;
  * @version 0.1.0-SNAPSHOT
  */
 @SpringBootApplication
+@ComponentScan("patrickcemper.cloudstarx")
 @EntityScan("patrickcemper.cloudstarx.entities")
 @EnableJpaRepositories("patrickcemper.cloudstarx.repositories")
 public class Application {
--- src/main/java/patrickcemper/cloudstarx/controllers/DefaultRestController.java
+++ src/main/java/patrickcemper/cloudstarx/controllers/DefaultRestController.java
@@ -1,10 +1,7 @@
 package patrickcemper.cloudstarx.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import patrickcemper.cloudstarx.entities.Place;
 import patrickcemper.cloudstarx.entities.Route;
 import patrickcemper.cloudstarx.repositories.PlaceRepository;
@@ -15,6 +12,7 @@ import patrickcemper.cloudstarx.services.RouteService;
 import java.util.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/rest")
 public class DefaultRestController {
 
